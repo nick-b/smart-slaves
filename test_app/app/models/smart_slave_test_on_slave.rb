0e9e71a55467d9f89937cfabfb1f2ab6e2f8aadd
Alexey Kovyrin <alexey@kovyrin.net>
2008-07-05 05:33:49 -0400

    Fixtures added (manual creates)

--- test_app/app/models/smart_slave_test_on_slave.rb
+++ test_app/app/models/smart_slave_test_on_slave.rb
@@ -1,4 +1,4 @@
 class SmartSlaveTestOnSlave < ActiveRecord::Base
-  establish_connection configuration[RAILS_ENV]['slave']
+  establish_connection :test_slave
   set_table_name 'smart_slave_tests'
 end
--- test_app/spec/smart_slaves_spec.rb
+++ test_app/spec/smart_slaves_spec.rb
@@ -7,6 +7,27 @@ module SmartSlavesHelper
       use_smart_slaves(opts)
     end
   end
+  
+  def create_fixture_record(klass, params)
+    rec = klass.new(params)
+    rec.id = params[:id]
+    rec.save
+  end
+  
+  def load_fixtures_data
+    SmartSlaveTestOnSlave.connection.execute("DELETE FROM smart_slave_tests")
+    create_fixture_record(SmartSlaveTestOnSlave, :name => 'test1', :id => 1)
+    create_fixture_record(SmartSlaveTestOnSlave, :name => 'test2', :id => 2)
+    create_fixture_record(SmartSlaveTestOnSlave, :name => 'test3', :id => 3)
+
+    SmartSlaveTest.connection.execute("DELETE FROM smart_slave_tests")
+    create_fixture_record(SmartSlaveTest, :name => 'test1', :id => 1)
+    create_fixture_record(SmartSlaveTest, :name => 'test2', :id => 2)
+    create_fixture_record(SmartSlaveTest, :name => 'test3', :id => 3)
+    create_fixture_record(SmartSlaveTest, :name => 'test4', :id => 4)
+    create_fixture_record(SmartSlaveTest, :name => 'test5', :id => 5)
+    create_fixture_record(SmartSlaveTest, :name => 'test6', :id => 6)
+  end
 end
 
 describe SmartSlaves do
@@ -35,14 +56,16 @@ end
 describe SmartSlaves, "with use_smart_slaves" do
   extend SmartSlavesHelper
   define_test_model :db => :slave
-  
+
+  load_fixtures_data
+
   before(:each) do
     puts "-----------------------------------------------------------------"
   end
 
   it "should retrieve and store in the cache our checkpoint id" do
     SmartSlaveTest.send(:checkpoint_value).should be_nil
-    SmartSlaveTest.find(5)
+    SmartSlaveTest.find(5).name.should == "test5"
     SmartSlaveTest.send(:checkpoint_value).should be(3)
   end  
 end
